d7bbc7427fad867d5319456e0ce9f4d0d3773934
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-03-24 12:44:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,63 +1,43 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
+  required_version = ">= 1.9.5"
 }
 
 provider "azurerm" {
-  subscription_id            = var.subscription_id
-  client_id                  = var.client_id
-  client_secret              = var.client_secret
-  tenant_id                  = var.tenant_id
-  use_msi                    = false  # Prevents Managed Identity issues
-  skip_provider_registration = true  # Speeds up Terraform runs
   features {}
+
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "location" { default = "East US" }
-
-resource "azurerm_resource_group" "main" {
-  name     = "rg-main"
-  location = var.location
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "mystorageacc123"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "main-vnet"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  address_space       = ["10.0.0.0/16"]
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "main-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-main"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+# Example resource to verify deployment
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"
+}
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
 }
