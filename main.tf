9a67f2d91733e27131a2503740e42cecdefae8f0
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-03-24 12:08:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  use_msi         = false  # Prevents 'az login' errors
+  subscription_id            = var.subscription_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
+  use_msi                    = false  # Prevents Managed Identity issues
+  skip_provider_registration = true  # Speeds up Terraform runs
   features {}
 }
 
@@ -21,17 +22,42 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "location" { default = "East US" }
-variable "prefix" { default = "firefly" }
 
 resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-rg"
+  name     = "rg-main"
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.prefix}storage"
+  name                     = "mystorageacc123"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "main-vnet"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "main-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-main"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
